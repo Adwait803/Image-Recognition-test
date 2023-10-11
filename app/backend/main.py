31f0efe6bee4dbc179c500c3c97551760edf8565
Adwait <86020693+Adwait803@users.noreply.github.com>
2023-10-11 21:40:56 +0530

    Update main.py

--- app/backend/main.py
+++ app/backend/main.py
@@ -1,45 +1,20 @@
-import asyncio
-import time
-import uuid
-from concurrent.futures import ProcessPoolExecutor
-from functools import partial
-import io
-
-import uvicorn
-from fastapi import File
-from fastapi import FastAPI
-from fastapi import UploadFile
-import numpy as np
+import streamlit as st
 from PIL import Image
-
-import config
 from clf import predict
 
+st.set_option('deprecation.showfileUploaderEncoding', False)
 
-app = FastAPI()
+st.title("Simple Image Classification App")
+st.write("")
 
-def read_imagefile(file) -> Image.Image:
-    image = Image.open(io.BytesIO(file))
-    return image
+file_up = st.file_uploader("Upload an image", type="jpg")
 
-@app.get("/")
-def read_root():
-    return {"message": "Welcome from the API"}
-
-
-
-
-
-@app.post("/a")
-async def predict_api(file: UploadFile = File(...)):
-    extension = file.filename.split(".")[-1] in ("jpg", "jpeg", "png")
-    if not extension:
-        return "Image must be jpg or png format!"
-    image = read_imagefile(await file.read())
-    prediction = predict(image)
-    return prediction
-
-
-
-if __name__ == "__main__":
-     uvicorn.run("main:app", host="localhost", port=8000)
+if file_up is not None:
+    image = Image.open(file_up)
+    st.image(image, caption='Uploaded Image.', use_column_width=True)
+    st.write("")
+    st.write("Just a second...")
+    labels = predict(file_up)
+    for i in labels:
+        st.write("It is a ", i[0])
+        break
